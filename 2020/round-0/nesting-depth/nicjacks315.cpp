89339ef5d90f5a75be2be86246667ce6c39cde61
nicjacks315 <80596121+nicjacks315@users.noreply.github.com>
2021-03-16 22:24:42 -0700

    Update nicjacks315.cpp

--- 2020/round-0/nesting-depth/nicjacks315.cpp
+++ 2020/round-0/nesting-depth/nicjacks315.cpp
@@ -1,8 +1,16 @@
+//Notes:
+//  1) I've done this before in writing my own XML parser:
+//  2) Take the difference in nesting depth and stick in appropriate number of open/close parentheses
+//  3) Only works for constructing your own nested data structure, doesnt work for reading as malformed structures break the algorithm
+//  4) Could be cleaner by using c-style strings, but in the name of time I used std::string
+//  5) This whole thing can written in about 3 lines in python
+
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
+//append chars
 void app(string& a, char c, int n) {
     for(int i=0;i<n;i++) {
         a+=c;
@@ -16,25 +24,29 @@ void solve(int c) {
     string out;
     int depth=0;
     
+    //handle first char
     int e=in[0]-'0';
     app(out,'(',e);
     depth+=e;
     out+=in[0];
 
+    //add chars based on delta depth
+    int dif;
     for(int i=1;i<in.length();i++) {
-        int dif=in[i]-in[i-1];
+        dif=in[i]-in[i-1];
         if(dif>0) {
             app(out,'(',dif);
         } else if(dif<0) {
-            app(out,')',abs(dif));
+            app(out,')',dif*-1);
         }
         out+=in[i];
         depth+=dif;
     }
     
+    //close out remaining
     app(out,')',depth);
 
-    cout<<"Case #"<<c<<": "<<out;
+    cout<<"Case #"<<c<<": "<<out<<'\n';
 }
 
 int main() {
